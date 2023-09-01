30203f949b9523490ecf039de25c8cd60eec770b
andt99 <andt@remitano.com>
2023-09-01 11:25:16 +0700

    update devnet -> testnet

--- src/experimental/break_flash_loans.ts
+++ src/experimental/break_flash_loans.ts
@@ -1,55 +1,80 @@
 /* eslint-disable no-unused-vars */
-import { Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js"
+import { getAssociatedTokenAddress } from "@solana/spl-token";
+import { Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js";
 import {
-  SOLEND_BETA_PROGRAM_ID,
   RelendMarket,
+  SOLEND_BETA_PROGRAM_ID,
   flashBorrowReserveLiquidityInstruction,
   flashRepayReserveLiquidityInstruction,
-} from "../../dist"
-import { getAssociatedTokenAddress } from "@solana/spl-token"
+} from "../../dist";
 
-const SOLANA_RPC_ENDPOINT = "https://api.devnet.solana.com"
+// const SOLANA_RPC_ENDPOINT = "https://api.devnet.solana.com"
+const SOLANA_RPC_ENDPOINT = "https://api.testnet.solana.com";
 
-const connection = new Connection(SOLANA_RPC_ENDPOINT)
+const connection = new Connection(SOLANA_RPC_ENDPOINT);
 
-const WALLET_PRIVATE_KEY: number[] = JSON.parse(process.env.WALLET_PRIVATE_KEY || "[]")
-export const USER_KEYPAIR = Keypair.fromSecretKey(Uint8Array.from(WALLET_PRIVATE_KEY))
+const WALLET_PRIVATE_KEY: number[] = JSON.parse(
+  process.env.WALLET_PRIVATE_KEY || "[]"
+);
+export const USER_KEYPAIR = Keypair.fromSecretKey(
+  Uint8Array.from(WALLET_PRIVATE_KEY)
+);
 
 const getATA = async (mintAddress: PublicKey, owner: PublicKey) => {
-  return getAssociatedTokenAddress(mintAddress, owner)
-}
+  return getAssociatedTokenAddress(mintAddress, owner);
+};
 
 const main = async () => {
-  const payer = USER_KEYPAIR
+  const payer = USER_KEYPAIR;
 
-  const lendingMarketKey = new PublicKey("HB1cecsgnFPBfKxEDfarVtKXEARWuViJKCqztWiFB3Uk")
+  const lendingMarketKey = new PublicKey(
+    "HB1cecsgnFPBfKxEDfarVtKXEARWuViJKCqztWiFB3Uk"
+  );
 
   // main pool
-  const market = await RelendMarket.initialize(connection, "beta", lendingMarketKey.toString())
+  const market = await RelendMarket.initialize(
+    connection,
+    "beta",
+    lendingMarketKey.toString()
+  );
 
-  const solReserve = market.reserves.find((res) => res.config.liquidityToken.symbol == "SOL")
-  const msolReserve = market.reserves.find((res) => res.config.liquidityToken.symbol == "mSOL")
+  const solReserve = market.reserves.find(
+    (res) => res.config.liquidityToken.symbol == "SOL"
+  );
+  const msolReserve = market.reserves.find(
+    (res) => res.config.liquidityToken.symbol == "mSOL"
+  );
 
   if (!solReserve || !msolReserve) {
-    console.log("Can't find both reserves.")
-    return
+    console.log("Can't find both reserves.");
+    return;
   }
-  console.log(solReserve!.config.address)
+  console.log(solReserve!.config.address);
 
   // turbo sol pool
-  const marketTurbo = await RelendMarket.initialize(connection, "beta", "Az4MpWtMcpENQZwbEbTnrgyd2qk3wsMwQXimadUiHSQp")
-  const solReserveTurbo = marketTurbo.reserves.find((res) => res.config.liquidityToken.symbol == "SOL") ?? null
+  const marketTurbo = await RelendMarket.initialize(
+    connection,
+    "beta",
+    "Az4MpWtMcpENQZwbEbTnrgyd2qk3wsMwQXimadUiHSQp"
+  );
+  const solReserveTurbo =
+    marketTurbo.reserves.find(
+      (res) => res.config.liquidityToken.symbol == "SOL"
+    ) ?? null;
 
   if (!solReserveTurbo) {
-    console.log("Can't find turbo sol reserve")
-    return
+    console.log("Can't find turbo sol reserve");
+    return;
   }
 
-  const solATA = await getATA(new PublicKey(solReserve.config.liquidityToken.mint), payer.publicKey)
+  const solATA = await getATA(
+    new PublicKey(solReserve.config.liquidityToken.mint),
+    payer.publicKey
+  );
 
-  console.log(solReserveTurbo.config.liquidityAddress)
+  console.log(solReserveTurbo.config.liquidityAddress);
 
-  const tx = new Transaction()
+  const tx = new Transaction();
 
   tx.add(
     flashBorrowReserveLiquidityInstruction(
@@ -102,12 +127,12 @@ const main = async () => {
       // program id
       SOLEND_BETA_PROGRAM_ID
     )
-  )
+  );
 
-  const sig = await connection.sendTransaction(tx, [payer])
-  console.log(`https://solscan.io/tx/${sig}`)
+  const sig = await connection.sendTransaction(tx, [payer]);
+  console.log(`https://solscan.io/tx/${sig}`);
 
-  await connection.confirmTransaction(sig)
-}
+  await connection.confirmTransaction(sig);
+};
 
-main()
+main();
