a85f672403205906b59c48cb52ab0cf51ec3da69
andt99 <andt@remitano.com>
2023-09-07 16:16:35 +0700

    update reserves RENEC

--- src/classes/constants.ts
+++ src/classes/constants.ts
@@ -4,14 +4,17 @@ export const WAD = "1".concat(Array(18 + 1).join("0"));
 export const WANG = "1".concat(Array(36 + 1).join("0"));
 export const U64_MAX = "18446744073709551615";
 
-export const SOLEND_PRODUCTION_PROGRAM_ID = new PublicKey(
+export const RELEND_PRODUCTION_PROGRAM_ID = new PublicKey(
   "So1endDq2YkqhipRh3WViPa8hdiSpxWy6z3Z6tMCpAo"
 );
-// export const SOLEND_DEVNET_PROGRAM_ID = new PublicKey("ALend7Ketfx5bxh6ghsCDXAoDrhvEmsXT3cynB6aPLgx")
-export const SOLEND_DEVNET_PROGRAM_ID = new PublicKey(
+// export const RELEND_DEVNET_PROGRAM_ID = new PublicKey("ALend7Ketfx5bxh6ghsCDXAoDrhvEmsXT3cynB6aPLgx")
+export const RELEND_DEVNET_PROGRAM_ID = new PublicKey(
   "2To33scbpBECFZwooXy2tWe13G1dpQA5Gem1LdYDkby5"
 );
-export const SOLEND_BETA_PROGRAM_ID = new PublicKey(
+export const RELEND_TESTNET_PROGRAM_ID = new PublicKey(
+  "2To33scbpBECFZwooXy2tWe13G1dpQA5Gem1LdYDkby5"
+);
+export const RELEND_BETA_PROGRAM_ID = new PublicKey(
   "BLendhFh4HGnycEDDFhbeFEUYLP4fXB5tTHMoTX8Dch5"
 );
 
@@ -19,13 +22,16 @@ export function getProgramId(environment?: string) {
   switch (environment) {
     case "mainnet-beta":
     case "production":
-      return SOLEND_PRODUCTION_PROGRAM_ID;
+      return RELEND_PRODUCTION_PROGRAM_ID;
       break;
     case "devnet":
-      return SOLEND_DEVNET_PROGRAM_ID;
+      return RELEND_DEVNET_PROGRAM_ID;
+      break;
+    case "testnet":
+      return RELEND_TESTNET_PROGRAM_ID;
       break;
     case "beta":
-      return SOLEND_BETA_PROGRAM_ID;
+      return RELEND_BETA_PROGRAM_ID;
       break;
   }
 
